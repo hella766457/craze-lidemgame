2202def6d3a56782e5c112bc63b92100911880a0
hella766457 <ki7676800@gmail.com>
2026-07-22 17:48:53 +0900

    Update 테스트1.js

--- songs/테스트1.js
+++ songs/테스트1.js
@@ -1,288 +1,102 @@
-if (typeof SONG_DATABASE === 'undefined') {
-    var SONG_DATABASE = [];
-}
+// 곡 및 채보 데이터
+if (typeof SONG_DATABASE === 'undefined') window.SONG_DATABASE = [];
 
 SONG_DATABASE.push({
-    title: "REFUSE (부분)",
-    bpm: 120,
-    bgm: "audio/end.mp3",
-    speed: 450,
-    notes: [
-    {
-        "time": 4.173,
-        "lane": 3
-    },
-    {
-        "time": 5.633,
-        "lane": 0
-    },
-    {
-        "time": 5.789,
-        "lane": 3
-    },
-    {
-        "time": 5.901,
-        "lane": 0
-    },
-    {
-        "time": 5.991,
-        "lane": 3
-    },
-    {
-        "time": 6.126,
-        "lane": 0
-    },
-    {
-        "time": 6.259,
-        "lane": 3
-    },
-    {
-        "time": 6.374,
-        "lane": 0
-    },
-    {
-        "time": 6.464,
-        "lane": 3
-    },
-    {
-        "time": 6.664,
-        "lane": 1
-    },
-    {
-        "time": 6.732,
-        "lane": 2
-    },
-    {
-        "time": 6.867,
-        "lane": 1
-    },
-    {
-        "time": 7.006,
-        "lane": 2
-    },
-    {
-        "time": 7.118,
-        "lane": 1
-    },
-    {
-        "time": 7.226,
-        "lane": 2
-    },
-    {
-        "time": 7.319,
-        "lane": 1
-    },
-    {
-        "time": 7.407,
-        "lane": 2
-    },
-    {
-        "time": 7.586,
-        "lane": 0
-    },
-    {
-        "time": 7.723,
-        "lane": 3
-    },
-    {
-        "time": 7.813,
-        "lane": 0
-    },
-    {
-        "time": 7.904,
-        "lane": 3
-    },
-    {
-        "time": 8.016,
-        "lane": 0
-    },
-    {
-        "time": 8.109,
-        "lane": 3
-    },
-    {
-        "time": 8.31,
-        "lane": 0
-    },
-    {
-        "time": 8.327,
-        "lane": 3
-    },
-    {
-        "time": 8.441,
-        "lane": 1
-    },
-    {
-        "time": 8.576,
-        "lane": 2
-    },
-    {
-        "time": 8.604,
-        "lane": 1
-    },
-    {
-        "time": 8.713,
-        "lane": 2
-    },
-    {
-        "time": 8.759,
-        "lane": 1
-    },
-    {
-        "time": 8.847,
-        "lane": 2
-    },
-    {
-        "time": 8.915,
-        "lane": 1
-    },
-    {
-        "time": 9.074,
-        "lane": 2
-    },
-    {
-        "time": 9.106,
-        "lane": 1
-    },
-    {
-        "time": 9.254,
-        "lane": 2
-    },
-    {
-        "time": 9.367,
-        "lane": 1
-    },
-    {
-        "time": 9.48,
-        "lane": 2
-    },
-    {
-        "time": 9.567,
-        "lane": 1
-    },
-    {
-        "time": 9.634,
-        "lane": 2
-    },
-    {
-        "time": 9.706,
-        "lane": 1
-    },
-    {
-        "time": 9.815,
-        "lane": 2
-    },
-    {
-        "time": 9.907,
-        "lane": 1
-    },
-    {
-        "time": 9.995,
-        "lane": 2
-    },
-    {
-        "time": 10.085,
-        "lane": 1
-    },
-    {
-        "time": 10.198,
-        "lane": 2
-    },
-    {
-        "time": 10.29,
-        "lane": 1
-    },
-    {
-        "time": 10.375,
-        "lane": 2
-    },
-    {
-        "time": 10.465,
-        "lane": 1
-    },
-    {
-        "time": 10.558,
-        "lane": 2
-    },
-    {
-        "time": 11.509,
-        "lane": 2
-    },
-    {
-        "time": 11.532,
-        "lane": 0
-    },
-    {
-        "time": 11.544,
-        "lane": 1
-    },
-    {
-        "time": 11.555,
-        "lane": 3
-    },
-    {
-        "time": 12.475,
-        "lane": 3
-    },
-    {
-        "time": 12.671,
-        "lane": 2
-    },
-    {
-        "time": 12.829,
-        "lane": 1
-    },
-    {
-        "time": 13.079,
-        "lane": 0
-    },
-    {
-        "time": 13.26,
-        "lane": 1
-    },
-    {
-        "time": 13.508,
-        "lane": 2
-    },
-    {
-        "time": 13.707,
-        "lane": 3
-    },
-    {
-        "time": 13.954,
-        "lane": 1
-    },
-    {
-        "time": 14.179,
-        "lane": 2
-    },
-    {
-        "time": 14.359,
-        "lane": 0
-    },
-    {
-        "time": 14.744,
-        "lane": 0
-    },
-    {
-        "time": 14.946,
-        "lane": 2
-    },
-    {
-        "time": 15.148,
-        "lane": 1
-    },
-    {
-        "time": 15.372,
-        "lane": 3
-    },
-    {
-        "time": 15.962,
-        "lane": 0
-    },
-    {
-        "time": 16.091,
-        "lane": 3
-    }
-]
+  id: "anzs_raid",
+  title: "안즈스 레이드 BGM",
+  bgm: "audio/song.mp3",
+  bpm: 120,
+  keys: 4,
+  difficulty: "Lv.3 하드",
+  speed: 480,
+  notes: [
+    { time: 0.5, lane: 3 },
+    { time: 0.5, lane: 2 },
+    { time: 0.5, lane: 1 },
+    { time: 0.5, lane: 0 },
+    { time: 1, lane: 3 },
+    { time: 1, lane: 2 },
+    { time: 1, lane: 1 },
+    { time: 1, lane: 0 },
+    { time: 1.375, lane: 0 },
+    { time: 1.375, lane: 1 },
+    { time: 1.375, lane: 2 },
+    { time: 1.375, lane: 3 },
+    { time: 1.5, lane: 2 },
+    { time: 1.5, lane: 0 },
+    { time: 1.5, lane: 1 },
+    { time: 1.5, lane: 3 },
+    { time: 1.75, lane: 3 },
+    { time: 1.75, lane: 2 },
+    { time: 1.75, lane: 0 },
+    { time: 1.75, lane: 1 },
+    { time: 1.875, lane: 3 },
+    { time: 1.875, lane: 2 },
+    { time: 1.875, lane: 1 },
+    { time: 1.875, lane: 0 },
+    { time: 2, lane: 2 },
+    { time: 2, lane: 3 },
+    { time: 2, lane: 0 },
+    { time: 2, lane: 1 },
+    { time: 2.125, lane: 3 },
+    { time: 2.125, lane: 2 },
+    { time: 2.125, lane: 0 },
+    { time: 2.125, lane: 1 },
+    { time: 2.75, lane: 0 },
+    { time: 2.875, lane: 1 },
+    { time: 3, lane: 2 },
+    { time: 3.125, lane: 3 },
+    { time: 3.25, lane: 3 },
+    { time: 3.375, lane: 2 },
+    { time: 3.5, lane: 1 },
+    { time: 3.625, lane: 0 },
+    { time: 3.75, lane: 0 },
+    { time: 3.875, lane: 1 },
+    { time: 4, lane: 2 },
+    { time: 4.125, lane: 3 },
+    { time: 4.625, lane: 3 },
+    { time: 4.75, lane: 1 },
+    { time: 5.125, lane: 0 },
+    { time: 5.125, lane: 2 },
+    { time: 5.625, lane: 3 },
+    { time: 5.75, lane: 3 },
+    { time: 5.875, lane: 3 },
+    { time: 6.375, lane: 3 },
+    { time: 6.5, lane: 3 },
+    { time: 6.625, lane: 3 },
+    { time: 7.375, lane: 1 },
+    { time: 7.625, lane: 3 },
+    { time: 8, lane: 0 },
+    { time: 8.625, lane: 3 },
+    { time: 8.875, lane: 3 },
+    { time: 8.875, lane: 0 },
+    { time: 9.125, lane: 3 },
+    { time: 9.25, lane: 1 },
+    { time: 9.375, lane: 3 },
+    { time: 9.625, lane: 3 },
+    { time: 9.625, lane: 0 },
+    { time: 9.875, lane: 3 },
+    { time: 10.5, lane: 0 },
+    { time: 10.625, lane: 1 },
+    { time: 10.75, lane: 2 },
+    { time: 10.875, lane: 3 },
+    { time: 11.25, lane: 0 },
+    { time: 11.375, lane: 1 },
+    { time: 11.5, lane: 2 },
+    { time: 11.625, lane: 3 },
+    { time: 12.5, lane: 0 },
+    { time: 12.5, lane: 3 },
+    { time: 12.625, lane: 1 },
+    { time: 12.625, lane: 2 },
+    { time: 12.75, lane: 0 },
+    { time: 12.75, lane: 3 },
+    { time: 13.5, lane: 1 },
+    { time: 13.5, lane: 2 },
+    { time: 13.625, lane: 0 },
+    { time: 13.625, lane: 3 },
+    { time: 13.75, lane: 2 },
+    { time: 13.75, lane: 1 },
+    { time: 13.875, lane: 1 },
+    { time: 13.875, lane: 2 }
+  ]
 });
